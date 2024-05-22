f42585e142c66189133bae53db777a384d8038d2
Tim Clark <timeeeee@gmail.com>
2024-05-22 17:03:09 -0400

    remove example features

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { useEffect } from "react"
 import "./App.css"
-import { Counter } from "./features/counter/Counter"
-import { Quotes } from "./features/quotes/Quotes"
 import { InItemList } from "./features/inItems/InItemList"
-import logo from "./logo.svg"
 
 const App = () => {
   return (
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,11 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 import { inItemsSlice } from "../features/inItems/inItemsSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice, quotesApiSlice, inItemsSlice)
+const rootReducer = combineSlices(inItemsSlice)
 // Infer the `RootState` type from the root reducer
 export type RootState = ReturnType<typeof rootReducer>
 
@@ -16,11 +14,13 @@ export type RootState = ReturnType<typeof rootReducer>
 export const makeStore = (preloadedState?: Partial<RootState>) => {
   const store = configureStore({
     reducer: rootReducer,
+    /*
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: getDefaultMiddleware => {
       return getDefaultMiddleware().concat(quotesApiSlice.middleware)
     },
+    */
     preloadedState,
   })
   // configure listeners using the provided defaults
