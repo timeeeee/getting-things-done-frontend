8582d4fac8c488f765dc45c6ecf58f3889c5d0ed
Tim Clark <timeeeee@gmail.com>
2024-05-30 10:57:44 -0400

    add router with one route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
 import "./App.css"
 import { InItemList } from "./features/inItems/InItemList"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <InItemList />,
+  }
+])
 
 const App = () => {
   return (
     <div className="App">
-      <InItemList />
+      <RouterProvider router={router} />
     </div>
   )
 }
