89f94bef1f2d11f45a2efdbeb6b98e500c990c91
Tim Clark <timeeeee@gmail.com>
2024-06-21 11:55:12 -0400

    use mantine tabs for project buckets

--- src/features/projects/ProjectList.tsx
+++ src/features/projects/ProjectList.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom"
 import type { EntityId } from "@reduxjs/toolkit"
 import { useDisclosure } from '@mantine/hooks';
-import { Modal, Button } from '@mantine/core';
+import { Modal, Tabs } from '@mantine/core';
+import { useState } from 'react'
 
+import type { Bucket } from "../../app/types"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import type { LoadingStatus } from "../../app/types"
 import {
-    selectProjectIds,
     selectProjectById,
+    selectProjectIdsByBucket,
     createProject,
     type ProjectData
 } from "./projectsSlice"
@@ -41,13 +43,48 @@ const ProjectPreview = ({ projectId }: {projectId: EntityId}) => {
     return <ProjectPreviewBody id={projectId} projectData={project} error={project.error} status={project.status} />
 }
 
+
+const ProjectListTab = ({ bucket }: {bucket: Bucket}) => {
+    const ids = useAppSelector(state => selectProjectIdsByBucket(state, bucket))
+    const pendingProject = useAppSelector(state => state.projects.pendingNewProject)
+
+    return (
+        <Tabs.Panel key={bucket} value={bucket}>
+            <ul>
+                {pendingProject !== undefined && pendingProject.bucket === bucket &&
+                    <li key="pending"><ProjectPreviewBody projectData={pendingProject} /></li>}
+
+                {ids.map(id => (
+                    <li key={id}><ProjectPreview projectId={id} /></li>
+                ))}
+            </ul>
+        </Tabs.Panel>
+    )
+}
+
+
+const buckets: Bucket[] = ["active", "complete", "maybe", "trash"]
+
+
 export const ProjectList = () => {
     const dispatch = useAppDispatch()
 
-    const projectIds = useAppSelector(selectProjectIds)
     const loadingStatus = useAppSelector(state => state.projects.loading)
     const error = useAppSelector(state => state.projects.error)
-    const pendingProject = useAppSelector(state => state.projects.pendingNewProject)
+
+    // todo: use history state to track selected tab
+    /*
+    const [activeTab, setActiveTab] = useReducer(
+        (tab: Bucket) => {
+            // todo: update history state
+            console.log(`changing tab to ${tab}`)
+            return tab
+        },
+        "maybe" 
+    )
+    */
+
+    const [activeTab, setActiveTab] = useState<string | null>("active")
 
     const [isNewProjectDialogOpen, { open: openNewProjectDialog, close: closeNewProjectDialog }] = useDisclosure()
 
@@ -58,8 +95,10 @@ export const ProjectList = () => {
 
     if (loadingStatus === "loading") return <LoadingSpinner />
 
-    if (loadingStatus === "failed") return <div className="error" >Failed to fetch projects: {error}</div>
+    if (loadingStatus === "failed") return <div className="error">Failed to fetch projects: {error}</div>
 
+    // todo: make sure pending bucket fits selected bucket
+    // todo: use router history state to remember which bucket we're on
     if (loadingStatus === "idle") return (
         <>
             <button onClick={openNewProjectDialog}>Create Project</button>
@@ -67,14 +106,13 @@ export const ProjectList = () => {
                 <ProjectForm onSave={handleSave} />
             </Modal>
             
-            <ul id="project-list">
-                {pendingProject !== undefined && <li key="pending"><ProjectPreviewBody projectData={pendingProject} /></li>}
-                {projectIds.map((id) =>
-                    <li key={id}>
-                        <ProjectPreview projectId={id} />
-                    </li>
-                )}
-            </ul>
+            <Tabs value={activeTab} onChange={setActiveTab}>
+                <Tabs.List>
+                    {buckets.map(bucket => <Tabs.Tab key={bucket} value={bucket}>{bucket}</Tabs.Tab>)}
+                </Tabs.List>
+
+                {buckets.map(bucket => <ProjectListTab bucket={bucket} />)}
+            </Tabs>
         </>
     )
 }
